74c6e419f22f179b468920be79584f700d22b1ae
Uksusov Kirill <uksusoff@yandex.ru>
2025-10-13 14:05:32 +0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ru.uksusov.rock63',
+  webDir: 'dist'
+};
+
+export default config;
